a2c6f9d09e585f0b4dab0145c90c9cb6a7eb0127
KuraiZoltan <kz1022@gmail.com>
2023-07-26 17:51:15 +0200

    Creating the db context for entity framework

---
+++ Data/MovieTheaterDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BárdiHomework.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BárdiHomework.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
