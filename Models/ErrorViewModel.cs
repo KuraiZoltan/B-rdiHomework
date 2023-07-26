c1b7bde0cb537ba385e8920907d3ed5f40de8345
KuraiZoltan <kz1022@gmail.com>
2023-07-26 18:29:25 +0200

    Re-adding file

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BárdiHomework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
