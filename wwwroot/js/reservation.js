c73a22cb80fcf00e6c019bc6591bd38cf50db331
KuraiZoltan <kz1022@gmail.com>
2023-07-27 09:18:26 +0200

    Adding event listeners and sending check reservation request

--- wwwroot/js/reservation.js
+++ wwwroot/js/reservation.js
@@ -1,3 +1,25 @@
 ﻿const seatOne = document.querySelector("#seat-one")
 const seatTwo = document.querySelector("#seat-two")
+const calendar = document.querySelector("#calendar")
 
+let dateTime;
+let reservation = {}
+
+calendar.addEventListener("change", e => {
+    dateTime = e.currentTarget.value
+    checkForReservation();
+})
+
+seatOne.addEventListener("click", () => {
+    reservation['seatOne'] = dateTime
+})
+
+seatTwo.addEventListener("click", () => {
+    reservation['seatTwo'] = dateTime
+})
+
+function checkForReservation() {
+    fetch('https://localhost:7289/Reservation/checkReservation')
+        .then(response => response.text())
+        .then(text => console.log(text))
+}
