b4c50c05dea81d7f0a08bc6bbb0ec989afc7bf5e
KuraiZoltan <kz1022@gmail.com>
2023-07-27 15:38:39 +0200

    Seat data model file implementation

---
+++ Models/SeatData.cs
@@ -0,0 +1,12 @@
+﻿namespace BárdiHomework.Models
+{
+    public class SeatData
+    {
+        public int Id { get; set; }
+        public string ReservedBy { get; set; }
+        public string SeatName { get; set; }
+        public string SeatStatus { get; set; }
+        public DateTime TimeOfReservation { get; set; }
+        public Boolean IsPaid { get; set; }
+    }
+}
