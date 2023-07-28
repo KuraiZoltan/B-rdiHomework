eb62d011d742628093e52950b96436ad74d74c2b
KuraiZoltan <kz1022@gmail.com>
2023-07-28 15:36:37 +0200

    Method for getting selected seats by seat name

--- Services/SeatService.cs
+++ Services/SeatService.cs
@@ -28,5 +28,33 @@ namespace BárdiHomework.Services
             }
             return seats;
         }
+
+        public async Task<IEnumerable<SeatData>> GetSeatsBySeatName(Dictionary<string, string> seatNumbers)
+        {
+            var seats = new List<SeatData>();
+            using var connection = new MySqlConnection("Server=localhost;User ID=Zolika1022;Password=Zolika1022;Database=seats");
+
+            await connection.OpenAsync();
+            foreach (var seatName in seatNumbers)
+            {
+                using var command = new MySqlCommand($"SELECT * FROM seats WHERE seat_number = '{seatName.Key}';", connection);
+                using var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    SeatData seat = new SeatData()
+                    {
+                        Id = (int)reader.GetValue(0),
+                        ReservedBy = (string)reader.GetValue(1),
+                        SeatStatus = reader.GetString("status"),
+                        SeatName = reader.GetString("seat_number"),
+                        TimeOfReservation = (DateTime)reader.GetValue(4),
+                        IsPaid = reader.GetBoolean("purchase_done")
+                    };
+                    seats.Add(seat);
+                }
+            }
+            
+            return seats;
+        }
     }
 }
