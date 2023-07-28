958b6f3f7b990d5c14e47d81494b614567d2db61
KuraiZoltan <kz1022@gmail.com>
2023-07-28 15:39:36 +0200

    Getting the selected seats and validating if they are available

--- Controllers/SeatController.cs
+++ Controllers/SeatController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BárdiHomework.Services;
 using BárdiHomework.Models;
+using System.Linq;
 
 namespace BárdiHomework.Controllers
 {
@@ -8,16 +9,33 @@ namespace BárdiHomework.Controllers
     [Route("[controller]")]
     public class SeatController : ControllerBase
     {
-        private SeatService _reservationService;
-        public SeatController(SeatService service) 
+        private SeatService _seatService;
+        private ReservationService _reservationService;
+        public SeatController(SeatService seatService, ReservationService reservationService) 
         {
-            _reservationService = service;
+            _seatService = seatService;
+            _reservationService = reservationService;
         }
         [HttpGet]
         [Route("getSeats")]
         public async Task<IEnumerable<SeatData>> GetSeats()
         {
-            return await _reservationService.GetSeats();
+            return await _seatService.GetSeats();
+        }
+
+        [HttpPost]
+        [Route("reserveSeats")]
+        public async Task<IActionResult> ReserveSeats([FromBody] Dictionary<string, string> seatNumbers)
+        {
+            var seats = await _seatService.GetSeatsBySeatName(seatNumbers);
+            foreach (var seat in seats)
+            {
+                if (seat.SeatStatus == "free")
+                {
+                    _reservationService.InitiateReservation(seat);
+                }
+            }
+            return Ok(seats);
         }
     }
 }
