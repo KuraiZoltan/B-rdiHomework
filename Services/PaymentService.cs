c52aea93e6da3c312dbb1626364ce85dda71946e
KuraiZoltan <kz1022@gmail.com>
2023-07-30 16:11:31 +0200

    Changing sql query with adding where clause on id

--- Services/PaymentService.cs
+++ Services/PaymentService.cs
@@ -9,7 +9,8 @@ namespace BárdiHomework.Services
         {
             using var connection = new MySqlConnection("Server=localhost;User ID=Zolika1022;Password=Zolika1022;Database=seats");
             await connection.OpenAsync();
-            using var command = new MySqlCommand("UPDATE seats SET reserved_by=@payedBy, status='occupied', purchase_done=1", connection);
+            using var command = new MySqlCommand("UPDATE seats SET reserved_by=@payedBy, status='occupied', purchase_done=1 WHERE id=@seatId", connection);
+            command.Parameters.AddWithValue("@seatId", seat.Id);
             command.Parameters.AddWithValue("@payedBy", payedBy);
             await command.ExecuteNonQueryAsync();
         }
