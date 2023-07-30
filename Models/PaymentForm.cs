53cfc2d929c46bfb2759746519a3472c925b1616
KuraiZoltan <kz1022@gmail.com>
2023-07-30 16:10:09 +0200

    Adding payment form to handle request body

---
+++ Models/PaymentForm.cs
@@ -0,0 +1,8 @@
+﻿namespace BárdiHomework.Models
+{
+    public class PaymentForm
+    {
+        public Dictionary<string, string> Seats { get; set; }
+        public string Email { get; set; }
+    }
+}
