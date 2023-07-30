a84c012b2d96bd9d1d61331c3fd7eff2b11bb5bf
KuraiZoltan <kz1022@gmail.com>
2023-07-30 16:10:43 +0200

    Implementing email service to send confirmation email

---
+++ Services/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using BárdiHomework.Models;
+
+namespace BárdiHomework.Services
+{
+    public class EmailService
+    {
+        public void SendConfirmationEmail(PaymentForm paymentForm)
+        {
+            
+            var seatsList = paymentForm.Seats.Keys.ToList();
+            string result = string.Join("", seatsList);
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("fakebboost@gmail.com"));
+            email.To.Add(MailboxAddress.Parse(paymentForm.Email));
+            email.Subject = "Seat Reservation";
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                
+                Text = $"Reservation has been made to seat(s): {result}"
+                
+            };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 465, MailKit.Security.SecureSocketOptions.SslOnConnect);
+            smtp.Authenticate("fakebboost@gmail.com", "gconlzshnpusjupa");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
