1560e6899e29ce439d04dc4227c061287a61739c
KuraiZoltan <kz1022@gmail.com>
2023-07-30 12:07:54 +0200

    Implementing payment for seats method

---
+++ Services/PaymentService.cs
@@ -0,0 +1,17 @@
+﻿using BárdiHomework.Models;
+using MySqlConnector;
+
+namespace BárdiHomework.Services
+{
+    public class PaymentService
+    {
+        public async Task PayForSeat(SeatData seat, string payedBy)
+        {
+            using var connection = new MySqlConnection("Server=localhost;User ID=Zolika1022;Password=Zolika1022;Database=seats");
+            await connection.OpenAsync();
+            using var command = new MySqlCommand("UPDATE seats SET reserved_by=@payedBy, status='occupied', purchase_done=1", connection);
+            command.Parameters.AddWithValue("@payedBy", payedBy);
+            await command.ExecuteNonQueryAsync();
+        }
+    }
+}
