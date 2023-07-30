7572a188b0362ee16d4df182c0f752db03634b43
KuraiZoltan <kz1022@gmail.com>
2023-07-30 12:08:38 +0200

    Adding pay for seats endpoint and reserve seat minor return changes

--- Controllers/SeatController.cs
+++ Controllers/SeatController.cs
@@ -11,10 +11,12 @@ namespace BárdiHomework.Controllers
     {
         private SeatService _seatService;
         private ReservationService _reservationService;
-        public SeatController(SeatService seatService, ReservationService reservationService) 
+        private PaymentService _paymentService;
+        public SeatController(SeatService seatService, ReservationService reservationService, PaymentService paymentService) 
         {
             _seatService = seatService;
             _reservationService = reservationService;
+            _paymentService = paymentService;
         }
         [HttpGet]
         [Route("getSeats")]
@@ -28,14 +30,31 @@ namespace BárdiHomework.Controllers
         public async Task<IActionResult> ReserveSeats([FromBody] Dictionary<string, string> seatNumbers)
         {
             var seats = await _seatService.GetSeatsBySeatName(seatNumbers);
+
             foreach (var seat in seats)
             {
                 if (seat.SeatStatus == "free")
                 {
                     await _reservationService.InitiateReservation(seat);
+                    return Ok(seats);
+                }
+                else
+                {
+                    return BadRequest("The seat(s) are already occupied.");
                 }
             }
-            return Ok(seats);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("payForSeats")]
+        public async Task PayForSeats([FromBody] Dictionary<string, string> seatNumbers, string payedBy)
+        {
+            var seats = await _seatService.GetSeatsBySeatName(seatNumbers);
+            foreach (var seat in seats)
+            {
+                await _paymentService.PayForSeat(seat, payedBy);
+            }
         }
     }
 }
